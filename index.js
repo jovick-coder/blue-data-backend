f518edc2497f418ad9acb01bcd1b56b0a4ff186d
Jovick-coder <victorjosiahm3@gmail.com>
2022-04-22 15:12:48 +0000

    added authorization middelwere

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const cors = require("cors");
 const fs = require("fs");
 const { default: mongoose } = require("mongoose");
+const jwt = require("jsonwebtoken");
 const userRouter = require("./router/userRouter");
+const historyRouter = require("./router/historyRouter");
+const userAccountRouter = require("./router/userAccountRouter");
+const User = require("./model/userModel");
 
 app.use(
   cors({
@@ -27,7 +31,35 @@ mongoose
     });
   })
   .catch((err) => console.log("Error->", err));
+
+async function authorization(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token || token === "") {
+    return res.status(401).send({ ok: false, message: "unAuthorized User" });
+  }
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const { uId, privilege } = decode;
+    const user = await User.findOne({ _id: uId, privilege: privilege });
+    if (!user)
+      return respond(res, 404, {
+        ok: false,
+        message: "Authorized User Not Found",
+      });
+
+    res.locals.userId = uId;
+    res.locals.userPrivilege = privilege;
+    // res.send(decode);
+    next();
+  } catch (error) {
+    res.send({ ok: false, message: "server error", error: error });
+  }
+}
 app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/history", authorization, historyRouter);
+app.use("/api/account", authorization, userAccountRouter);
 
 // D16SbQEFWNflGfMO
