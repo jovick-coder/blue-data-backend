f60fb2cacc0c3172235503def955f2bd6b1ff244
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-09 22:09:38 +0000

    added a delete route in historyRouter

--- router/historyRouter.js
+++ router/historyRouter.js
@@ -7,7 +7,10 @@ historyRouter.get("/", async (req, res) => {
   if (userPrivilege === 1 || 2) {
     try {
       const history = await History.find({ uId: userId });
-      if (!history) return res.status(404).send("history not found");
+      if (!history)
+        return res
+          .status(404)
+          .send({ ok: false, message: "history not found" });
       return res.send({ ok: true, data: history });
     } catch (error) {
       return res.send({
@@ -21,12 +24,46 @@ historyRouter.get("/", async (req, res) => {
   if (userPrivilege === 3 || 4) {
     try {
       const history = await History.find();
-      if (!history) return res.status(404).send("history not found");
+      if (!history)
+        return res
+          .status(404)
+          .send({ ok: false, message: "history not found" });
       res.send(res.locals);
     } catch (error) {
       res.send({ ok: false, message: "Server error", error: error + "." });
     }
     return;
+  }
+});
+
+historyRouter.delete("/:id", async (req, res) => {
+  const { userId, userPrivilege } = res.locals;
+  const id = req.params.id;
+  // if params is * delete all
+  if (id === "*") {
+    try {
+      const history = await History.deleteMany({ uId: userId });
+      if (!history) {
+        res.status(404).send({ ok: false, message: "history not found" });
+        return;
+      }
+      return res.send({ ok: true, data: history });
+    } catch (error) {
+      res.send({ ok: false, message: "Server error", error: error + "." });
+    }
+  }
+  if (id === "") {
+    res.send({ ok: false, message: "Invalid Parameter" });
+    return;
+  }
+  // Delete one
+  try {
+    const history = await History.findOneAndDelete({ _id: id, uId: userId });
+    if (!history)
+      return res.status(404).send({ ok: false, message: "history not found" });
+    res.send({ ok: true, data: history });
+  } catch (error) {
+    res.send({ ok: false, message: "Server error", error: error + "." });
   }
 });
 
