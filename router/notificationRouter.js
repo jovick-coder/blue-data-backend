e33d23b2f8ed3124f1b526966638b43b8bfd8a4c
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-10 08:02:50 +0000

    imported user model

--- router/notificationRouter.js
+++ router/notificationRouter.js
@@ -1,5 +1,6 @@
 const notificationRouter = require("express").Router();
 const Notification = require("../model/notificationModel.js");
+const User = require("../model/userModel.js");
 
 notificationRouter.get("/", async (req, res) => {
   const { userId, userPrivilege } = res.locals;
@@ -74,24 +75,43 @@ notificationRouter.delete("/:id", async (req, res) => {
 
 notificationRouter.post("/", async (req, res) => {
   const { userId, userPrivilege } = res.locals;
+  let { message, category } = req.body;
 
   if (userPrivilege !== 3 && userPrivilege !== 4) {
     res.status(401).send({ ok: false, message: "UnAuthorized User " });
     return;
   }
 
-  if (req.body.message === "") {
+  if (message === "") {
     res.status(400).send({ ok: false, message: "Message empty" });
     return;
   }
+  if (!category || category === "") {
+    category === 0;
+  }
 
-  const notification = new Notification({
-    aId: userId,
-    message: req.body.message,
-    privilege: userPrivilege,
-  });
+  // try {
 
+  // } catch (error) {
+  //   return res.status(500).send({
+  //     ok: false,
+  //     message: "Error Finding Admins name",
+  //     error: error,
+  //   });
+  // }
   try {
+    const user = await User.findOne({ _id: userId, privilege: userPrivilege });
+    if (!user)
+      return res.status(404).send({
+        ok: false,
+        message: "Admin Not Found",
+      });
+    const notification = new Notification({
+      adminName: user.userName,
+      message: req.body.message,
+      privilege: userPrivilege,
+      category: category,
+    });
     // const notification =
     await notification.save();
 
