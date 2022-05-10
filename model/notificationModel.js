bd308f6f0ba0f30546cf69d112ab3f08efa0f01c
Jovick-coder <victorjosiahm3@gmail.com>
2022-05-10 08:02:40 +0000

    updated model

--- model/notificationModel.js
+++ model/notificationModel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema({
-  aId: { type: String, required: true },
+  adminName: { type: String, required: true },
   message: { type: String, required: true },
   privilege: {
     type: Number,
     required: true,
   },
+  date: { type: Date, required: true, default: Date.now() },
+  category: { type: Number, required: true },
+  seen: { type: Boolean, required: true, default: false },
 });
 // const newLocal = "notification";
 module.exports = mongoose.model("Notification", notificationSchema);
