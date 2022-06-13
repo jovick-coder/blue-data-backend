550ef106a650923c2423a47152b013dd3c1d2bcd
Jovick-coder <victorjosiahm3@gmail.com>
2022-06-13 15:19:53 +0100

    removed the admin right to see all history

--- router/historyRouter.js
+++ router/historyRouter.js
@@ -4,36 +4,35 @@ const History = require("../model/historyModel");
 historyRouter.get("/", async (req, res) => {
   const { userId, userPrivilege } = res.locals;
   // for user and resellers
-  if (userPrivilege === 1 || 2) {
-    try {
-      const history = await History.find({ uId: userId });
-      if (!history)
-        return res
-          .status(404)
-          .send({ ok: false, message: "history not found" });
-      return res.send({ ok: true, data: history });
-    } catch (error) {
-      return res.send({
-        ok: false,
-        message: "Server error",
-        error: error + ".",
-      });
-    }
+  // if (userPrivilege === 1 || userPrivilege === 2) {
+  try {
+    const history = await History.find({ uId: userId });
+    if (!history)
+      return res.status(404).send({ ok: false, message: "history not found" });
+    return res.send({ ok: true, data: history });
+  } catch (error) {
+    return res.send({
+      ok: false,
+      message: "Server error",
+      error: error + ".",
+    });
   }
-  // for admin and super admin
-  if (userPrivilege === 3 || 4) {
-    try {
-      const history = await History.find();
-      if (!history)
-        return res
-          .status(404)
-          .send({ ok: false, message: "history not found" });
-      res.send(res.locals);
-    } catch (error) {
-      res.send({ ok: false, message: "Server error", error: error + "." });
-    }
-    return;
-  }
+  // }
+  // // for admin and super admin
+  // if (userPrivilege === 3 || userPrivilege === 4) {
+  //   try {
+  //     const history = await History.find();
+  //     if (!history)
+  //       return res
+  //         .status(404)
+  //         .send({ ok: false, message: "history not found" });
+  //     // res.send(res.locals);
+  //     return res.send({ ok: true, data: history });
+  //   } catch (error) {
+  //     res.send({ ok: false, message: "Server error", error: error + "." });
+  //   }
+  //   return;
+  // }
 });
 
 historyRouter.delete("/:id", async (req, res) => {
